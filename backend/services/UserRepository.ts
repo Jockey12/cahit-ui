426ed4d0f8c03a2524ddf22554cc563683b004cc
Onur <jockey101@proton.me>
2026-08-29 00:04:26 -0400

    Update/Create TS files

--- backend/services/UserRepository.ts
+++ backend/services/UserRepository.ts
@@ -1,6 +1,6 @@
 import pool from "../config/db";
 
-export async function getUserById(id) {
+export async function getUserById(id: number) {
   const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
   return result.rows[0];
 }
