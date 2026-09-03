eef83956fe7d41445e97a1a164160a58d75ba0aa
Onur <jockey101@proton.me>
2026-09-03 11:14:49 -0400

    index

--- backend/index.ts
+++ backend/index.ts
@@ -5,7 +5,10 @@ import "dotenv/config";
 const app = express().use(cors());
 const router = express.Router();
 const port = 8080;
-
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url} from ${req.host}`);
+  next();
+});
 app.post("/chat", (req, res) => {
   res.send("Content-Type");
 });
